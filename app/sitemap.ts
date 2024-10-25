dd539703fac6401252c3cc1f727baacec2a07c01
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-10-25 18:01:38 +0900

    sitemap added

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,27 +2,13 @@ import posts from "content/posts"
 
 export const baseUrl = 'https://nextjs-blog-template.pages.dev'
 
-export async function sitemap() {
-  let blogs = posts.map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.date,
-  }))
-
-  let routes = ['', '/blog'].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }))
-
-  return [...routes, ...blogs]
-}
-
 export default async function sitemap2() {
   let blogs = posts.map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.date,
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  let routes = ['', '/blog', '/guide', '/guide/kr'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
