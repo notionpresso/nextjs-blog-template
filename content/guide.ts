b65cfe8b23917f67a746b64cad59c79a023c4eac
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-10-25 17:39:23 +0900

    english guide

--- content/guide.ts
+++ content/guide.ts
@@ -1,10 +1,18 @@
 import kr_guide from '../notion-data/126ce18c-fd83-80a5-8260-d757c56405b2.json'
+import en_guide from '../notion-data/12ace18c-fd83-8071-b3a5-dd8d21da61cf.json'
 
 export const kr = {
   title: "노션을 활용해서 나만의 블로그 만들기 대공개",
   content: kr_guide as { blocks: any[] },
   description: "NotionPresso 블로그 템플릿 가이드",
   image: undefined,
-} 
+}
 
-export default kr;
+export const en = {
+  title: "NotionPresso Blog Template Guide",
+  content: en_guide as { blocks: any[] },
+  description: "NotionPresso 블로그 템플릿 가이드",
+  image: undefined,
+}
+
+export default en;
