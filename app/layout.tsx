8a7dcd132a4425b04d4848fd47d569c5201035c6
JoSuwon <60573972+JoSuwon@users.noreply.github.com>
2024-11-21 11:42:06 +0900

    fix: body margin 값 수정 (기존 576~1024 구간 ui 치우침) (#4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto text-black bg-white dark:text-white dark:bg-black">
+      <body className="antialiased max-w-xl mx-4 mt-8 sm:mx-auto text-black bg-white dark:text-white dark:bg-black">
         <ThemeProvider attribute='class' defaultTheme='light' >
           <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
             <Navbar />
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '38rem',
+    }
   },
   plugins: [],
   darkMode: 'class'
