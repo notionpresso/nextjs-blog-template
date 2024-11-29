03eff4b370a8308f382f8671cb46f03872fe4a40
Serim Min <97885933+anonymousRecords@users.noreply.github.com>
2024-11-29 10:58:48 +0900

    fix: giscus config (#7)

--- components/comment.tsx
+++ components/comment.tsx
@@ -1,22 +1,24 @@
-'use client'
-import Giscus from "@giscus/react";
+// To enable comments:
+// 1. Uncomment this component
+// 2. Update repo, repoId, and categoryId with your own giscus settings
+// 3. Visit https://giscus.app to get your settings
 
-export default function Comment() {
-  return (
-    <Giscus
-      id="comments"
-      repo="notionpresso/nextjs-blog-template"
-      repoId="R_kgDONDy8gg"
-      category="Announcements"
-      categoryId="DIC_kwDONDy8gs4CjpSV"
-      mapping="pathname"
-      strict="0"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="bottom"
-      theme="preferred_color_scheme"
-      lang="ko"
-      loading="lazy"
-    />
-  )
-}
+// export default function Comment() {
+//   return (
+//     <Giscus
+//       id="comments"
+//       repo="" 
+//       repoId=""
+//       category="Announcements"
+//       categoryId=""
+//       mapping="pathname"
+//       strict="0"
+//       reactionsEnabled="1"
+//       emitMetadata="0"
+//       inputPosition="bottom"
+//       theme="preferred_color_scheme"
+//       lang="ko"
+//       loading="lazy"
+//     />
+//   )
+// }
